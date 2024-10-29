60a4c65c7a60ec1ef10d49eb74875e8203d52f11
anggis <anggisdar@gmail.com>
2024-10-29 16:12:35 +0700

    Menambahkan file variable.go

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"pertama/calculation"
-)
-
 func main() {
-	fmt.Println("Halo, belajar Golang")
 
-	result := calculation.Add(8, 9)
+	//fmt.Println("Halo, belajar Golang")
+	//sentence := TestAja()
 
-	fmt.Println(result)
+	//fmt.Println(sentence)
+
+	//fmt.Println("Halo, belajar Golang") /
+
+	//result := calculation.Add(8, 9)
+
+	//fmt.Println(result)
 }
---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var language string
+	language = "Golang"
+	age := 23
+
+	fmt.Println(language)
+	fmt.Println(age)
+}
