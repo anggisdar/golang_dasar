d2630c173fc33904941bd6a5c93da576ed3f7218
anggis <anggisdar@gmail.com>
2025-01-04 09:50:08 +0700

    git add 'for range'

--- for/main.go
+++ for/main.go
@@ -8,9 +8,25 @@ func main() {
 		fmt.Println("saya adalah backend golang", i)
 	}
 
-	title := "Golang the best language"
-	for index, letter := range title {
-		fmt.Println("index:", index, "letter:", string(letter))
-	}
+	// title := "Golang the best language"
+	// for index, letter := range title {
+	// 	fmt.Println("index:", index, "letter:", string(letter))
+	// }
 
+	// // tanpa index
+	// title := "Golang the best language"
+
+	// for _, letter := range title {
+	// 	fmt.Println("letter:", string(letter))
+	// }
+
+	title := "Golang the best language"
+
+	for index, letter := range title {
+		letterString := string(letter)
+
+		if letterString == "a" || letterString == "i" || letterString == "u" || letterString == "e" || letterString == "0" {
+			fmt.Println("index:", index, "letter:", string(letter))
+		}
+	}
 }
