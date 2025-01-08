e4b7aa6ec7affa788ecef87824fdccdd0644b2f6
anggis <anggisdar@gmail.com>
2025-01-08 17:16:11 +0700

    feat: iterate and delete from map

--- map/main.go
+++ map/main.go
@@ -5,10 +5,27 @@ import (
 )
 
 func main() {
-	var myMap map[string]int
-	myMap = map[string]int{}
+	// var myMap map[string]int
+	// myMap = map[string]int{}
 
-	myMap["go"] = 2
-	fmt.Println(myMap)
+	// myMap["go"] = 2
+	// fmt.Println(myMap)
+
+	myMap := map[string]string{
+		"golang":     "static language",
+		"javascript": "dynamic language",
+	}
+
+	for key, value := range myMap {
+		fmt.Println("key:", key, "value:", value)
+	}
+
+	fmt.Println("============")
+
+	delete(myMap, "javascript")
+
+	for key, value := range myMap {
+		fmt.Println("key:", key, "value:", value)
+	}
 
 }
