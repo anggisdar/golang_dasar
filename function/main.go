992d783f60d1300c6c57acc53a27ad1df63d80cb
anggis <anggisdar@gmail.com>
2025-01-13 14:17:21 +0700

    feat : git add function int

--- function/main.go
+++ function/main.go
@@ -5,11 +5,28 @@ import (
 )
 
 func main() {
-	printMyResult()
-	printMyResult()
-	printMyResult()
+	// "function string"
+	// printMyResult("saya sedang")
+	// printMyResult("belajar golang")
+
+	// "function int"
+	result := add(10, 20)
+	fmt.Println(result)
+
+	// sentence := printMyResult("saya sedang")
+	// fmt.Println(sentence)
 }
 
-func printMyResult() {
-	fmt.Println("go!!!")
+// pemanggilan yang berada di satu package main dengan awalan huruf kecil
+func add(number int, numberTwo int) int {
+	result := number + numberTwo
+	return result
 }
+
+// func printMyResult(sentence string) {
+// 	fmt.Println(sentence)
+
+// func printMyResult(sentence string) string {
+// 	newSentence := sentence + "belajar golang"
+// 	return newSentence // mengembalikan nilai dengan "return" yang menghasilkan output
+// }
