828e9f408c2670616f9284aef81ea8c59b8a8bb9
anggis <anggisdar@gmail.com>
2025-01-20 12:56:50 +0700

    feat : git add pointer refence & direference

--- pointer/main.go
+++ pointer/main.go
@@ -3,7 +3,35 @@ package main
 import "fmt"
 
 func main() {
+
+	/* numberA := 5
+	numberB := &numberA // & alamat memory dalam bentuk heksadesimal
+
+	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB) // * mengembalikan nilai "proses derefrencing"
+
+	*numberB = 10 // mendeklarasikan ulang * mengembalikan nilai "proses derefrencing"
+
+	 fmt.Println(numberA)*/
+
+	// refence & direference
+	var numberA int = 5
+	var numberB *int = &numberA // reference
+
+	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB) // dereference
+
+	numberA = 20
+
+	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB)
+
+	// dengan array
 	var parts = [5]string{"mb", "subboard", "mainFpc", "receiver", "speaker"}
 	partsPointer := &parts
 	fmt.Println("array as pointer", partsPointer)
+
 }
