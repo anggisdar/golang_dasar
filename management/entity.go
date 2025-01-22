40b6e1d494d4b8bda413c5236c64baf4fae8047f
anggis <anggisdar@gmail.com>
2025-01-22 19:37:40 +0700

    feat : git add exported struct

---
+++ management/entity.go
@@ -0,0 +1,17 @@
+package management
+
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	IsActive  bool
+}
+
+// embedded struct
+type Group struct {
+	Name        string
+	Admin       User
+	Users       []User
+	IsAvailable bool
+}
