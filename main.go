5886c2758a8e5bd1b3bcfdf0a0de13a22c4f5d49
anggis <anggisdar@gmail.com>
2025-01-22 19:38:31 +0700

    feat : git add main exported struct

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"pertama/calculation"
+	// "pertama/calculation"
+	"pertama/management"
 )
 
 func main() {
@@ -12,9 +13,61 @@ func main() {
 
 	// fmt.Println(sentence)
 
-	fmt.Println("Halo, belajar Golang")
+	// fmt.Println("Halo, belajar Golang")
 
-	result := calculation.Add(8, 9)
+	// result := calculation.Add(8, 9)
 
-	fmt.Println(result)
+	// fmt.Println(result)
+	// cara 1
+	user := management.User{}
+	user.ID = 1
+	user.FirstName = "james"
+	user.LastName = "bond"
+	user.Email = "jamesbond@james.com"
+	user.IsActive = true
+
+	fmt.Println(user)
+
+	//cara 2 horizontal mirip map
+	userTwo := management.User{ID: 2, FirstName: "jack", LastName: "chan", Email: "jackchan@jack.co.id", IsActive: true}
+	fmt.Println(userTwo)
+
+	// cara 3 vertikal "urutannya bisa acak"
+	userThree := management.User{
+		Email:     "jacksparrow@jack.xyz",
+		ID:        3,
+		FirstName: "jack",
+		LastName:  "sparrow",
+		IsActive:  true,
+	}
+
+	fmt.Println(userThree)
+
+	// cara 4 syarat harus id yang pertama
+	user4 := management.User{4, "jack", "ma", "jackma@jack.org", true}
+	user5 := management.User{2, "jack", "aa", "jackma@jack.org", true}
+	// fmt.Println(userFour)
+
+	// displayUser := displayUser(userFour)
+	// fmt.Println(displayUser)
+
+	//embedded struct
+	users := []management.User{user4, user5}
+	group := management.Group{"Gamer", user, users, true}
+
+	displayGroup(group)
+}
+
+// embedded struct
+func displayGroup(group management.Group) {
+	fmt.Printf("Name: %s", group.Name)
+	fmt.Println("")
+	fmt.Printf("Member count : %d", len(group.Users))
+
+}
+
+// struct sebagai parameter
+func displayUser(user management.User) string {
+	return fmt.Sprintf("Name : %s %s, Email : %s", user.FirstName, user.LastName, user.Email)
+
 }
