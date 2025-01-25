53f9730304ecbabf00fd58f4501093b1e76619eb
anggis <anggisdar@gmail.com>
2025-01-25 10:20:12 +0700

    feat : update manipulation  using pointer

--- pointer/main.go
+++ pointer/main.go
@@ -15,23 +15,37 @@ func main() {
 
 	 fmt.Println(numberA)*/
 
-	// refence & direference
-	var numberA int = 5
-	var numberB *int = &numberA // reference
+	// // refence & direference
+	// var numberA int = 5
+	// var numberB *int = &numberA // reference
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB) // dereference
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB) // dereference
 
-	numberA = 20
+	// numberA = 20
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
 
-	// dengan array
-	var parts = [5]string{"mb", "subboard", "mainFpc", "receiver", "speaker"}
-	partsPointer := &parts
-	fmt.Println("array as pointer", partsPointer)
+	// // dengan array
+	// var parts = [5]string{"mb", "subboard", "mainFpc", "receiver", "speaker"}
+	// partsPointer := &parts
+	// fmt.Println("array as pointer", partsPointer)
 
+	number := 5
+	fmt.Println("Alamat memory :", &number)
+	fmt.Println("Nilai awal :", number)
+
+	change(&number, 100)
+
+	fmt.Println("Nilai akhir :", number)
+	fmt.Println("alamat memory:", &number)
+}
+
+func change(old *int, new int) {
+	*old = new
+	fmt.Println("Alamat memory:", old)
+	fmt.Println("nilai di dalam function", *old)
 }
