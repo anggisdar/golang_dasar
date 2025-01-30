c04d462619b6d17d5b2377471fe3a466346793d2
anggis <anggisdar@gmail.com>
2025-01-30 15:39:55 +0700

    feat : git update interface study case

--- interface/main.go
+++ interface/main.go
@@ -1,6 +1,11 @@
 package main
 
-type Luas interface { // kontrak yang harus dipenuhi
+import (
+	"fmt"
+)
+
+// type Luas interface { // kontrak yang harus dipenuhi
+type BangunDatar interface { // kontrak yang harus dipenuhi
 	HitungLuas() int // method
 }
 
@@ -21,5 +26,31 @@ func (PersegiPanjang PersegiPanjang) HitungLuas() int {
 	return PersegiPanjang.Panjang * PersegiPanjang.Lebar
 }
 
+// tes
+
+type Tes struct {
+	Angka int
+}
+
+func (tes Tes) HitungLuas() int {
+	return 1000
+}
+
+// masalah
 func main() {
+	PersegiPanjang := PersegiPanjang{Panjang: 6, Lebar: 5}
+	luas := SeberapaLuas(PersegiPanjang)
+	fmt.Println("Luas Persegi Panjang :", luas)
+
+	persegi := Persegi{Sisi: 4}
+	luas = SeberapaLuas(persegi)
+	fmt.Println("Luas Persegi:", luas)
+
+	tes := Tes{Angka: 5}
+	luas = SeberapaLuas(tes)
+	fmt.Println("luas Tes", luas)
+}
+
+func SeberapaLuas(bangunDatar BangunDatar) int {
+	return bangunDatar.HitungLuas()
 }
